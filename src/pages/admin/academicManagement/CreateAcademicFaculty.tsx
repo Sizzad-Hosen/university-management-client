2247fb3b4515b24edb0ea81e77f4019775473c6a
Sizzad Hosen <sizzadhosenofficial@gmail.com>
2025-05-30 20:53:03 +0600

    create academic faculty

--- src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
+++ src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
+
+import PHInput from '@/app/components/form/FInput';
+import PHForm from '@/app/components/form/From';
+import { Button, message } from 'antd';
+import React from 'react';
 
 const CreateAcademicFaculty = () => {
-  return (
-    <div>CreateAcademicFaculty</div>
-  )
-}
+  const onSubmit = (data: any) => {
+    console.log(data);
+    message.success('Academic faculty created successfully!');
+  };
 
-export default CreateAcademicFaculty
+  return (
+    
+    <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+      <h1 style={{ marginBottom: '24px' }}>Create Academic Faculty</h1>
+      <PHForm onSubmit={onSubmit}>  
+        <PHInput
+          type="text" 
+          name="name" 
+          label="Faculty Name"  
+        />
+        
+        <Button 
+          type="primary" 
+          htmlType="submit"
+          style={{ marginTop: '16px' }}
+        >
+          Create Faculty
+        </Button>
+      </PHForm>
+    </div>
+  );
+};
+
+export default CreateAcademicFaculty;
