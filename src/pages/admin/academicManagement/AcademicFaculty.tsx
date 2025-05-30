698a56c38f8bdcc94372b2624eadc383e27f1cac
Sizzad Hosen <sizzadhosenofficial@gmail.com>
2025-05-31 00:02:28 +0600

    get all academic faculty data in the table

--- src/pages/admin/academicManagement/AcademicFaculty.tsx
+++ src/pages/admin/academicManagement/AcademicFaculty.tsx
@@ -1,8 +1,7 @@
 'use client'
-import { useGetAllDepartmentQuery } from '@/redux/features/admin/academicManagement.api';
-import { TQueryParam } from '@/types/global';
+import {  useGetAllFacultyQuery } from '@/redux/features/admin/academicManagement.api';
+
 import { Button, Table, TableColumnsType, TableProps } from 'antd';
-import { useState } from 'react';
 
 
 export type TTableData = {
@@ -14,39 +13,25 @@ export type TTableData = {
 
 const AcademicFaculty = () => {
 
-  const [params, setParams] = useState<TQueryParam[]>([]);
+
   
-  const { data: departmentData, isLoading, isFetching } = useGetAllDepartmentQuery(params);
+  const { data:facultyData, isLoading, isFetching } = useGetAllFacultyQuery();
 
   // Prepare table data
-  const tableData = departmentData?.data?.map((department: any) => ({
-    key: department._id,
-    name: department.name,
-    academicFaculty: department.academicFaculty._id,
-    facultyName: department.academicFaculty.name
+  const tableData = facultyData?.data?.map((faculty: any) => ({
+    key: faculty._id,
+    name: faculty.name,
+ 
   })) || [];
 
-  // Prepare faculty filters from the data
-  const facultyFilters = Array.from(
-    new Set(tableData.map(item => item.facultyName))
-  ).map(faculty => ({
-    text: faculty,
-    value: faculty,
-  }));
 
   const columns: TableColumnsType<TTableData> = [
     {
-      title: 'Department Name',
+      title: 'Faculty Name',
       dataIndex: 'name',
       key: 'name',
     },
-    {
-      title: 'Faculty',
-      dataIndex: 'facultyName',
-      key: 'facultyName',
-      filters: facultyFilters,
-      onFilter: (value, record) => record.facultyName === value,
-    },
+   
     {
       title: 'Action',
       key: 'action',
