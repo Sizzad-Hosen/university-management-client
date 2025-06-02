f20d5f5a1cbad784adb9922fae09737413955002
Sizzad Hosen <sizzadhosenofficial@gmail.com>
2025-06-02 17:44:19 +0600

    load registered semester successfully show UI

--- src/redux/features/admin/courseManagement.api.ts
+++ src/redux/features/admin/courseManagement.api.ts
@@ -1,8 +1,39 @@
 import { baseApi } from "@/redux/api/baseApi";
+import { TAcademicSemester } from "@/types/academicManagement.type";
+import { TSemester } from "@/types/courseManagement.type";
+import { TQueryParam, TResponseRedux } from "@/types/global";
 
 
 const courseManagementApi = baseApi.injectEndpoints({
     endpoints:(builder)=>({
+
+                  getAllRegisteredSemester: builder.query({
+
+              query: (args) => {
+                console.log(args);
+                const params = new URLSearchParams();
+        
+                if (args) {
+                  args.forEach((item: TQueryParam) => {
+                    params.append(item.name, item.value as string);
+                  });
+                }
+        
+                return {
+                  url: '/semester-registrations',
+                  method: 'GET',
+                   params: params,
+                };
+              },
+              transformResponse: (response: TResponseRedux<TSemester[]>) => {
+                return {
+                  data: response.data,
+                  meta: response.meta,
+                };
+              },
+            }),
+
+
         addRegisteredSemester:builder.mutation({
             query:(data)=>({
                 url:'/semester-registrations/create-semester-registration',
@@ -14,4 +45,4 @@ const courseManagementApi = baseApi.injectEndpoints({
 })
 
 
-export const {useAddRegisteredSemesterMutation} = courseManagementApi;
+export const {useAddRegisteredSemesterMutation,useGetAllRegisteredSemesterQuery} = courseManagementApi;
---
+++ src/types/courseManagement.type.ts
@@ -0,0 +1,16 @@
+import { TAcademicSemester } from "./academicManagement.type";
+
+
+
+export type TSemester ={
+    _id:string;
+    academicSemester:TAcademicSemester;
+    status:string;
+    startDate:string;
+    endDate:string;
+    minCredit:number;
+    maxCredit:number;
+    cretedAt:string;
+    updatedAt:string;
+
+}
